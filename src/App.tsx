b97501257c09bd89262ff4178bf0f7ce54f052ee
saviojks <saviooribe@gmail.com>
2023-02-26 15:47:58 -0300

    Loading animation

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='container'>
+      <div className='ring' />
+      <div className='ring' />
+      <div className='ring' />
+      <p>loading...</p>
+
+    </div>
+  )
+}
+
+export default App
